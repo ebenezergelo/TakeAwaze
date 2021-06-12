70a0b679ecd034010e8b18962f80fec20b43cb8c
Reecelaz <85742854+Reecelaz@users.noreply.github.com>
2021-06-12 11:45:06 +0200

    Reece

--- app/src/main/java/com/example/takeawaze/MainActivity.java
+++ app/src/main/java/com/example/takeawaze/MainActivity.java
@@ -14,8 +14,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public String Testing()
-    {
-        return "Ebby is a whore";
-    }
 }

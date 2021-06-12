751a997517581e6ff8ca38526e253aaecbcbcbcb
ebendotcom <ebygelo@icloud.com>
2021-06-12 18:01:12 +0200

    Eben - UI Components 80% done

---
+++ app/src/main/java/com/example/takeawaze/StaffSignUp.java
@@ -0,0 +1,13 @@
+package com.example.takeawaze;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StaffSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_sign_up);
+    }
+}

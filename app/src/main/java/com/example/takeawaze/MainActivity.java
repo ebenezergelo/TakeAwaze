97c7517bb01a6cb41334dbb208947d68d782b0d0
Reecelaz <85742854+Reecelaz@users.noreply.github.com>
2021-06-15 14:54:36 +0200

    Reece

--- app/src/main/java/com/example/takeawaze/MainActivity.java
+++ app/src/main/java/com/example/takeawaze/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.takeawaze;
 
+import android.content.Context;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,10 +21,11 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<Staff> staffs = new ArrayList<>();
     public ArrayList<Order> orders = new ArrayList<>();
     PHPRequest php = new PHPRequest();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_orders);
         try {
             popUsers();
         } catch (IOException e) {
@@ -41,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
     public void popUsers() throws IOException, JSONException {
-     String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/popuser.php");
-     JSONArray ja = new JSONArray(json);
+        String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/popuser.php");
+        JSONArray ja = new JSONArray(json);
         String USER_PHONE;
         String USER_NAME;
         for (int i = 0; i < ja.length(); i++) {
@@ -54,13 +63,14 @@ public class MainActivity extends AppCompatActivity {
             users.add(usr);
         }
     }
+
     public void popStaffs() throws IOException, JSONException {
-     String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/popstaff.php");
-     JSONArray ja = new JSONArray(json);
-         String STAFF_NUM;
-         String STAFF_NAME;
-         String RESTAURANT;
-         String STAFF_PASSWORD;
+        String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/popstaff.php");
+        JSONArray ja = new JSONArray(json);
+        String STAFF_NUM;
+        String STAFF_NAME;
+        String RESTAURANT;
+        String STAFF_PASSWORD;
         for (int i = 0; i < ja.length(); i++) {
             JSONObject jo = ja.getJSONObject(i);
             STAFF_NUM = jo.getString("STAFF_NUM");
@@ -71,16 +81,17 @@ public class MainActivity extends AppCompatActivity {
             staffs.add(stf);
         }
     }
+
     public void popOrders() throws IOException, JSONException {
-     String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/poporder.php");
-     JSONArray ja = new JSONArray(json);
-         int ORDER_NUM;
-         String USER_NAME;
-         String STAFF_NAME;
-         String RESTAURANT;
-         String STATUS;
-         String TIME;
-         int RATING;
+        String json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/poporder.php");
+        JSONArray ja = new JSONArray(json);
+        int ORDER_NUM;
+        String USER_NAME;
+        String STAFF_NAME;
+        String RESTAURANT;
+        String STATUS;
+        String TIME;
+        int RATING;
         for (int i = 0; i < ja.length(); i++) {
             JSONObject jo = ja.getJSONObject(i);
             ORDER_NUM = jo.getInt("ORDER_NUM");
@@ -94,13 +105,68 @@ public class MainActivity extends AppCompatActivity {
             orders.add(ordr);
         }
     }
-    public void userLogin(View v)
-    {
-     setContentView(R.layout.activity_user_login);
+
+    public void userLogin(View v) {
+        Button loginBtn = (Button) findViewById(R.id.logIN);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_user_login);
+                boolean custLoginToken = false;
+                EditText n = (EditText) findViewById(R.id.custName);
+                EditText p = (EditText) findViewById(R.id.userPhone);
+                String custLoginName = n.getText().toString();
+                String custLoginPhone = p.getText().toString();
+                String json = null;
+                JSONArray ja = null;
+                try {
+                    json = php.getRequest("https://lamp.ms.wits.ac.za/home/s2345362/popuser.php");
+                    ja = new JSONArray(json);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+                //JSONArray ja = new JSONArray(json);
+                String USER_PHONE = "";
+                String USER_NAME = "";
+                for (int i = 0; i < ja.length(); i++) {
+                    JSONObject jo = null;
+                    try {
+                        jo = ja.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        USER_PHONE = jo.getString("USER_PHONE");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        USER_NAME = jo.getString("USER_NAME");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    if (custLoginName.equals(USER_NAME) && custLoginPhone.equals(USER_PHONE)) {
+                        custLoginToken = true;
+                        break;
+                    }
+                }
+                if (custLoginToken) {
+                    setContentView(R.layout.activity_user_login);
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Invalid Login Details :(";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
     }
-    public void staffLogIn(View v)
-    {
-     setContentView(R.layout.activity_main_screen);
+
+    public void staffLogIn(View v) {
+        setContentView(R.layout.activity_main_screen);
     }
 
 

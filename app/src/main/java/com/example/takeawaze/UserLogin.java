84a8c91b294365b7c3138668da010112371c92b6
Reecelaz <85742854+Reecelaz@users.noreply.github.com>
2021-06-15 16:42:38 +0200

    Reece

--- app/src/main/java/com/example/takeawaze/UserLogin.java
+++ app/src/main/java/com/example/takeawaze/UserLogin.java
@@ -1,6 +1,8 @@
 package com.example.takeawaze;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,5 +21,13 @@ public class UserLogin extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        Button loginBtn = (Button) findViewById(R.id.logIN);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }

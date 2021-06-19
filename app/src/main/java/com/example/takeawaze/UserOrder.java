4ba80967058d64dffc081c158da6932284e97947
ebendotcom <ebygelo@icloud.com>
2021-06-19 15:37:24 +0200

    Eben - Completed

--- app/src/main/java/com/example/takeawaze/UserOrder.java
+++ app/src/main/java/com/example/takeawaze/UserOrder.java
@@ -16,7 +16,7 @@ public class UserOrder extends AppCompatActivity {
     UserDashboard u;
     Order order = u.rateOrder;
     int ordRating;
-    TextView rating = (TextView) findViewById(R.id.rating);
+    TextView rating;
     final int ordNum = order.getORDER_NUM();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,7 @@ public class UserOrder extends AppCompatActivity {
         TextView staName = (TextView) findViewById(R.id.staName);
         TextView rest = (TextView) findViewById(R.id.rest);
         TextView time = (TextView) findViewById(R.id.ordTime);
+        rating = (TextView) findViewById(R.id.rating);
         String rt = "";
         int ordRating= order.isRATING();
         if(ordRating==1)
@@ -42,48 +43,49 @@ public class UserOrder extends AppCompatActivity {
         time.setText(order.getTIME());
         rating.setText(rt);
     }
-//    public void like(View v)throws IOException
-//    {
-//        update(ordNum, 1);
-//        order.setRATING(1);
-//        String rt = "👍🏾";
-//        rating.setText(rt);
-//    }
-//    public void unlike(View v)throws IOException
-//    {
-//        update(ordNum, 0);
-//        order.setRATING(0);
-//        String rt = "👎🏾";
-//        rating.setText(rt);
-//    }
-//    public void update(int ORDER_NUM, int RATING)
-//            throws IOException {
-//        OkHttpClient client = new OkHttpClient();
-//        Request request = new Request.Builder()
-//                .url("https://lamp.ms.wits.ac.za/home/s2345362/rating.php" + "?num="
-//                        + ORDER_NUM + "&rating=" + RATING)
-//                .build();
-//        client.newCall(request).enqueue(new Callback() {
-//
-//            @Override
-//            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                Context context = getApplicationContext();
-//                CharSequence text = "New order creation failed :(";
-//                int duration = Toast.LENGTH_LONG;
-//                Toast toast = Toast.makeText(context, text, duration);
-//                toast.show();
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, final Response response) throws IOException {
-//                // ... check for failure using isSuccessful before proceeding
-//                if (!response.isSuccessful()) {
-//                    throw new IOException("Unexpected code " + response);
-//                }
-//
-//            }
-//        });
-//    }
+    public void like(View v)throws IOException
+    {
+        update(ordNum, 1);
+        order.setRATING(1);
+        String rt = "👍🏾";
+        rating.setText(rt);
+        
+    }
+    public void unlike(View v)throws IOException
+    {
+        update(ordNum, 0);
+        order.setRATING(0);
+        String rt = "👎🏾";
+        rating.setText(rt);
+    }
+    public void update(int ORDER_NUM, int RATING)
+            throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://lamp.ms.wits.ac.za/home/s2345362/rating.php" + "?num="
+                        + ORDER_NUM + "&rating=" + RATING)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Context context = getApplicationContext();
+                CharSequence text = "New order creation failed :(";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // ... check for failure using isSuccessful before proceeding
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+            }
+        });
+    }
     
 }
